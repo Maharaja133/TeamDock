dea2f37cc22a3481b4c16112f5ad8447d90cf6e5
Maharaja Prabhu <prabhumaharaja2@gmail.com>
2026-03-02 07:44:37 +0530

    vercel.json update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
-// src/api/axios.js (or wherever your instance is)
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
-  withCredentials: true // This automatically attaches the httpOnly cookie
+  baseURL: `/api`,
+  withCredentials: true 
 });
 
 export default instance;
